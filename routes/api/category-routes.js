4d151235b1bb678151d29f15f234241875d0947a
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-10-03 23:15:30 -0600

    Product Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,7 +61,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!category) {
-      res.status(404).json({ message: 'No library card found with that id!' });
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
     res.status(200).json(category);
